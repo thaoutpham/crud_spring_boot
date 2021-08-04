465395036845640f8f954650b778a33538898e30
thaoutpham <dongxuanhien65.dx@gmail.com>
2021-08-04 11:38:28 +0700

    crud spring boot

---
+++ src/main/java/com/democrud/repository/ICustomerRepository.java
@@ -0,0 +1,10 @@
+package com.democrud.repository;
+
+
+import com.democrud.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ src/main/java/com/democrud/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.democrud.service.customer;
+
+import com.democrud.model.Customer;
+import com.democrud.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
